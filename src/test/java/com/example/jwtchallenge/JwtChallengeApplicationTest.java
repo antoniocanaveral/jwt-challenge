a1e0a811218be817eaa775ee37511b29bd51bd0e
antoniocanaveral <antoniocanaveral@gmail.com>
2025-06-11 10:51:40 -0500

    Sonar

---
+++ src/test/java/com/example/jwtchallenge/JwtChallengeApplicationTest.java
@@ -0,0 +1,30 @@
+package com.example.jwtchallenge;
+
+import org.junit.Test;
+
+public class JwtChallengeApplicationTest {
+    @Test
+    public void testExamPart1() {
+
+    }
+
+    @Test
+    public void testInit() {
+        
+    }
+
+    @Test
+    public void testLogin() {
+
+    }
+
+    @Test
+    public void testMain() {
+
+    }
+
+    @Test
+    public void testSearchStudent() {
+
+    }
+}
